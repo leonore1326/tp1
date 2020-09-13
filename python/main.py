b6f8e58852b49fd7601b9b693808812e3d09774f
leonore1326 <71006829+leonore1326@users.noreply.github.com>
2020-09-13 23:17:18 +0200

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,7 +1,8 @@
 from numpy import random
+import time
 
-MIN_NUMBER = 0
-MAX_NUMBER = 100
+MIN_NUMBER = 1
+MAX_NUMBER = 50
 
 def game():
     ''' The game in itself    
@@ -10,30 +11,44 @@ def game():
     # The random number to guess
     r = random.randint(MIN_NUMBER,MAX_NUMBER)
     found = False
+    k=0
     
     # (Eternal) loop
     while not found:
         
-        entry = input("\nEnter a number between "+str(MIN_NUMBER)+" and "+str(MAX_NUMBER)+": ")
-        while not entry.isdigit():
-            print("please input a number")
-            entry = input("\nEnter a number between "+str(MIN_NUMBER)+" and "+str(MAX_NUMBER)+": ")
-
-        entry = int(entry)
-        # Condition on what to do
-        if entry == r:
-            print("\n\nGood job, it was "+str(r)+"!!!")
+        if k==7:
             found=True
-        elif entry>r:
-            print("You're too high!")
-        else:
-            print("A bit more?")
+            print("you too slow... you lost !")
+        else :
+            entry = input("\nEnter a number between "+str(MIN_NUMBER)+" and "+str(MAX_NUMBER)+": ")
+            while not entry.isdigit():
+                print("please input a number")
+                entry = input("\nEnter a number between "+str(MIN_NUMBER)+" and "+str(MAX_NUMBER)+": ")
+    
+            entry = int(entry)
+            # Condition on what to do
+            if entry<MIN_NUMBER or entry>MAX_NUMBER :
+                print("You should pay attention to the rules...")
+                time.sleep(10)
+            if entry == r:
+                print("\n\nGood job, it was "+str(r)+"!!!")
+                found=True
+            elif abs(r-entry)<=10 :
+                print("you're getting close, but...")
+            if entry>r:
+                print("You're too high!")
+            if entry<r:
+                print("A bit more?")
+            k=k+1
     	
  
 # Start the game only if you wish
-playerwish = input("Hello, want to play a game? ")
-if playerwish in ["yes", "y"] :
-	game()
-	
+#playerwish = input("Hello, want to play a game? ")
+#if playerwish in ["yes", "y"] :
+boul=True
+while boul :
+    print("\n\nLet's start a game...")
+    game()
 
+	
 
